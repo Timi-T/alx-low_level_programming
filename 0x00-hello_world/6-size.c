63ee558ebf944f9c048e91bd08ca5a96c836132f
Timi-T <timiopebode@gmail.com>
2021-08-26 13:53:47 -0700

    j

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	int longlonginttype;
 	float floattype;
 
-	printf("size of char: ", sizeof(chartype));
-	printf("size of int: ", sizeof(inttype));
-	printf("size of long: ", sizeof(longtype));
-	printf("size of long long int: ", sizeof(longlonginttype));
-	printf("size of float: ", sizeof(floattype));
+	printf("size of char: \n", sizeof(chartype));
+	printf("size of int: \n", sizeof(inttype));
+	printf("size of long: \n", sizeof(longtype));
+	printf("size of long long int: \n", sizeof(longlonginttype));
+	printf("size of float: \n", sizeof(floattype));
 	return (0);
 }
