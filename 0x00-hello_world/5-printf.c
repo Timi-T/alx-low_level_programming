e810f320a5b306535967c8021b86c0d738f7b30b
Timi-T <timiopebode@gmail.com>
2021-08-26 11:56:39 -0700

    printf5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/*code to print out a sentence*/
+/*
+ *code to print out a sentence
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
---
+++ 0x00-hello_world/main1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("tail c");
+  return (0);
+    }
