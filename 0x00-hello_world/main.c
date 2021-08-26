c7f086b8682835446a1d6d06b1b7e80d663a5512
Timi-T <timiopebode@gmail.com>
2021-08-26 12:02:42 -0700

    tab

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  printf("tail c");
-  return (0);
-    }
+	printf("tail c");
+	return (0);
+}
