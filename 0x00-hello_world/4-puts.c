51eb16baf1d93748e78b387b0ba368791d1943bb
Timi-T <timiopebode@gmail.com>
2021-08-26 05:49:08 -0700

    puts2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main ()
+int main()
 {
   char sentence[] = {'"','P','r','o','g','r','a','m','m','i','n','g',' ','i','s',' ','l','i','k','e',' ','b','u','i','l','d','i','n','g',' ','a',' ','m','u','l','t','i','l','i','n','g','u','a','l',' ','p','u','z','z','l','e','\n'};
   puts(sentence);
