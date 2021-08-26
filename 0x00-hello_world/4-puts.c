0acad72f5a210a9b19c0a681beec2ba76ebd2553
Timi-T <timiopebode@gmail.com>
2021-08-26 12:40:59 -0700

    comment and tab 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main()
 {
-  char sentence[50,2] = {'"',"Programming is like building a multilingual puzzle\n"};
-    puts(sentence);
-  return (0);
+	char sentence[50,2] = {'"',"Programming is like building a multilingual puzzle\n"};
+	puts(sentence);
+	return (0);
 }
