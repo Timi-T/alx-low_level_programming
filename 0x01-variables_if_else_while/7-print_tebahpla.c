0846dfd2cadc4e2dd7816045e0666cef7329af97
Timi-T <timiopebode@gmail.com>
2021-08-27 11:19:08 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return - always 0 (success)
+ */
+int main(void)
+{
+	int alp;
+	for (alp = 'z'; alp >= 'a'; alp--)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
+
