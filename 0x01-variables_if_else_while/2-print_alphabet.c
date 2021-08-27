e3dcab08fe25acaf479c38afbcc9785fd10429b6
Timi-T <timiopebode@gmail.com>
2021-08-27 07:57:42 -0700

    braces and removed space

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
 	char c;
 
@@ -13,5 +13,5 @@ int main(void)
 		putchar(c);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
