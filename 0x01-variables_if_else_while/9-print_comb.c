5bc23670089bd2e79eacf838f21602fc6328366f
Timi-T <timiopebode@gmail.com>
2021-08-29 12:15:39 -0700

    think

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i < 57)
+		{
+			putchar(i);
+			putchar(44);
+			putchar(32);
+		}
+		else 
+		{
+			putchar(i);
+		}
+	}
+	return (0);
+}
