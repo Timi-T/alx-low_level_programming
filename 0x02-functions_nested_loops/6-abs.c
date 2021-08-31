51fcd9f94cc2568c2addfad65aff456c9e407f3d
Timi-T <timiopebode@gmail.com>
2021-08-31 13:45:22 -0700

    k

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - to check if islower
+ *
+ * @c:  input from user
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int);
+{
+	int c;
+
+	if (c < 0)
+	{
+		c = (-1 * c);
+	}
+	else 
+	{
+		c = c;
+	}
+}
