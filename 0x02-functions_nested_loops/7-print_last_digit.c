00d1dd110f0523bd57e2b295d35b4b299b1f15a4
Timi-T <timiopebode@gmail.com>
2021-08-31 13:55:12 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - to check if islower
+ *
+ * @c:  input from user
+ *
+ * @i: lastdigit
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int c)
+{
+	int i;
+
+	i = c % 10;
+	_putchar(i);
+	return (0);
+}
