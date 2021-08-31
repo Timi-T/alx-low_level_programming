2c9f9ee197b47554dce889db26e879fce12cd80f
Timi-T <timiopebode@gmail.com>
2021-08-31 12:49:02 -0700

    k

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int print_sign(int n)
+{
+	if (c < 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	if (c == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	if (c > 0)
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
