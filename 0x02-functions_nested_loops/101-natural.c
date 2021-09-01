43e4f75628c2c98d85ae076019c56ab527ae08c6
Timi-T <timiopebode@gmail.com>
2021-09-01 07:57:37 -0700

    k

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,22 +2,22 @@
 /**
  * main - Entry point
  *
- * @n: numbers
  *
- * return: always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n;
+	int i;
 
 	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3 == 0) || (n % 5 == 0))
 		{
-			n += n; 
+			i = i + n;
 		}
 	}
-	printf("%d \n", n);
+	printf("%d \n", i);
 	return (0);
 }
