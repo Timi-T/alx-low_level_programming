af16a9667f4d7d68ef01aab8961fffa2d13a0ed3
Timi-T <timiopebode@gmail.com>
2021-09-01 13:06:01 -0700

    k

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,16 +1,28 @@
-#Include "main.h"
-/*
+#include "main.h"
+/**
+ * main - Entry point
  *
- *
- *
+ * Return: Always 0 (Success)
  */
 
 void times_table(void)
 {
 	int i;
+	int j;
+	int k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (j = 48; j <=57;
+		for (k = 48; k <= 57; k++)
+		{
+			j = i * k;
+		_putchar(j);
+			if (i < 57)
 			{
-
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+}	
