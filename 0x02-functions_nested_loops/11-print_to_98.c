a95fa1228cbf86c5a14a561be7dab29ff44c36bc
Timi-T <timiopebode@gmail.com>
2021-09-01 07:14:49 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function to print to 98
+ *
+ * @n: input from user
+ *
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			printf(n);
+			if (n != 98)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+	else if (n >= 98)
+		for (n = n; n > 98; n--)
+		{
+			printf(n);
+			if (n != 98)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+	}
+}
