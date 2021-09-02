cfd4fdc987ca693f2956452e5026554552f2b48f
Timi-T <timiopebode@gmail.com>
2021-09-02 04:37:44 -0700

    k

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _isupper - function to check case
+ *
+ * Return: Always 0
+ *
+ * @c:
+ */
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (c = i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
