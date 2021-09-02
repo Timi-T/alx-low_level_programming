ded64209f66588bd04a5a5c2a5bdc422a8dd507f
Timi-T <timiopebode@gmail.com>
2021-09-02 05:13:11 -0700

    k

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _isdigit - function to check digits
+ *
+ * Return: always 0
+ *
+ * @c: input from user
+ */
+
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,6 +13,7 @@ int add(int a, int b);
 void print_to_98(int n);
 void times_table(void);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
 
