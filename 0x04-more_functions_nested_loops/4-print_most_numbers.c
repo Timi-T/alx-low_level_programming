82dc0e11a8393188699d478d39283c7602ba0806
Timi-T <timiopebode@gmail.com>
2021-09-03 04:00:51 -0700

    k

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - function to print most numbers
+ *
+ * Return: always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
