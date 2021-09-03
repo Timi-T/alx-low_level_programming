e0b00e51ae5d33139ebcc1bb19006db2321ca4f1
Timi-T <timiopebode@gmail.com>
2021-09-03 11:44:10 -0700

    k

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && !(i % 5 == 0))
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			if (i % 5 == 0 && !(i % 3 == 0))
+			{
+				printf("Buzz ");
+			}
+			else if (i % 5 == 0 && (i % 3 == 0))
+			{
+				printf("Fizzbuzz ");
+			}
+			else
+			{
+				printf("%d ", i);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
