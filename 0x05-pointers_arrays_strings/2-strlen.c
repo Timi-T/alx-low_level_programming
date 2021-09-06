0743f69eb1177f00ba22e34fd40918ef467a0f2f
Timi-T <timiopebode@gmail.com>
2021-09-06 04:02:05 -0700

    lenth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function to print length of string
+ *
+ * @s: pointer for the string
+ *
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - function to print out a string
+ *
+ * @str: string to be printed out
+ *
+ * Return: always 0
+ */
+
+int _putchar(char c);
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i <= 
+	_putchar(*str[i]);
+	_putchar('\n');
+}
