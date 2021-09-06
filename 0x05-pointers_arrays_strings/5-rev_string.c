c692a87cfec8a0a6947c523ad9c69d04b69d626e
Timi-T <timiopebode@gmail.com>
2021-09-06 12:36:17 -0700

    k

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ void rev_string(char *s)
 	int i;
 	int j;
 	int k;
-	
+
 	for (j = 0; s[j] != 0; j++)
 	{
 	}
@@ -23,7 +23,6 @@ void rev_string(char *s)
 		for (i = j - 1; i >= 0;  i--)
 		{
 			s[k] = s[i];
-			s[i] = s[k];
 		}
 	}
 	_putchar('\n');
