15e7b085693e819cf2bcaac0cc475d907cd13bea
Timi-T <timiopebode@gmail.com>
2021-09-06 04:54:13 -0700

    k

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
  * _puts - function to print out a string
  *
  * @str: string to be printed out
- *
+ * @c: string
  * Return: always 0
  */
 
@@ -12,9 +12,9 @@ void _puts(char *str)
 {
 	int i;
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
+	for (i = 0; str[i] != '\0'; i++)
+	{
 		_putchar(str[i]);
-        }
+	}
 	_putchar('\n');
 }
