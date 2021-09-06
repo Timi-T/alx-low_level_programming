f8165cd3ab58c4463d9a53074ec562d8894caef6
Timi-T <timiopebode@gmail.com>
2021-09-06 04:47:23 -0700

    k

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,35 +1,20 @@
 #include "main.h"
 /**
  * _puts - function to print out a string
- * _strlen - function to print length of string
  *
  * @str: string to be printed out
- * @s: pointer for the string
  *
  * Return: always 0
  */
 
 int _putchar(char c);
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-
-	}
-	return (i);
-}
-
 void _puts(char *str)
 {
 	int i;
-	int a;
 
-	a = _strlen(str);
-	for (i = 0; i < a; i++)
-	{
+        for (i = 0; str[i] != '\0'; i++)
+        {
 		_putchar(str[i]);
-	}
+        }
 	_putchar('\n');
 }
