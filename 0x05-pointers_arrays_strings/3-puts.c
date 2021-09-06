9aca53c91eba98d3e3ba850bfe1796357a4113be
Timi-T <timiopebode@gmail.com>
2021-09-06 04:24:39 -0700

    k

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,10 +5,13 @@
  * @str: string to be printed out
  *
  * Return: always 0
+ *
+ * _strlen - function to print length of string
+ *
+ * @s: pointer for the string
  */
 
 int _putchar(char c);
-
 int _strlen(char *s)
 {
 	int i;
@@ -26,7 +29,7 @@ void _puts(char *str)
 	int a;
 
 	a = _strlen(str);
-	for (i = 0; i <= a; i++)
+	for (i = 0; i < a; i++)
 	{
 		_putchar(str[i]);
 	}
