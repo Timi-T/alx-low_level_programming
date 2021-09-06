3457f66b89e13f8d65a9a2db121d3aa0b68ffe49
Timi-T <timiopebode@gmail.com>
2021-09-06 11:23:02 -0700

    k

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,12 +14,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
-		if (i < n)
+		printf("%d", a[i]);
+		if (i < (n - 1))
 		{
 			printf(", ");
 		}
 	}
+	printf("\n");
 }
