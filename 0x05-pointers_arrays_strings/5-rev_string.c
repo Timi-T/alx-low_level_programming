4cbb0927189b90d3b4a4e91d31f683db07f4a888
Timi-T <timiopebode@gmail.com>
2021-09-06 12:43:14 -0700

    l

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,12 +18,10 @@ void rev_string(char *s)
 	for (j = 0; s[j] != 0; j++)
 	{
 	}
-	for (k = 0; k < j; k++)
+	
+	for (i = j - 1; i >= 0;  i--)
 	{
-		for (i = j - 1; i >= 0;  i--)
-		{
-			_putchar(s[i]);
-		}
+		s [k] = s[i]);
 	}
 	_putchar('\n');
 }
