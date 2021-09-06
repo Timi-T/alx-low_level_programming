b1ab1a4e2c5f78d5d22b99ee1a96bf09c7edb1f6
Timi-T <timiopebode@gmail.com>
2021-09-06 13:31:37 -0700

    l

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,8 +3,7 @@
 /**
  * _strcpy - function to copy from source to destination
  *
- * @dest: destination
- * @src: source
+ * @c: string
  *
  * Return: always 0
  */
@@ -13,7 +12,8 @@ int _putchar(char c);
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-
+	
+	*dest = '\0';
 	for (i = 0; src[i] != 0; i++)
 	{
 		dest[i] = src[i];
