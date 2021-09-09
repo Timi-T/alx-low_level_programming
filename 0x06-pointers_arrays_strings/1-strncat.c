9c2d6cf459c90706e0797cdd63c1c28c02ec42e3
Timi-T <timiopebode@gmail.com>
2021-09-09 00:34:05 -0700

    k

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - function to join 2 strings
+ *
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len;
+	int j;
+
+	for (len = 0; dest[len] != '\0'; len++)
+	{
+	}
+	for (j = 0; j < n; j++, len++)
+	{
+		dest[len] = src[j];
+	}
+	return (dest);
+}
