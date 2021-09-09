7bf66ea13cf00353f31b0c3184ec5bc82368fc5f
Timi-T <timiopebode@gmail.com>
2021-09-09 07:07:34 -0700

    k

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,21 +8,21 @@
 
 char *string_toupper(char *c)
 {
-        int i;
-        int j;
-        char *p;
+	int i;
+	int j;
+	char *p;
 
-        p = c;
-        for (i = 0; c[i] != '0'; i++)
-        {
-        }
-        for (j = 0; (j < i && c[j] != '\n'); j++)
-        {
-                if ((c[j] >= 97) && (c[j] <=122))
-                {
-                        p[j] = c[j] - 32;
-                        c[j] = p[j];
-                }
-        }
-        return (c);
+	p = c;
+	for (i = 0; c[i] != '0'; i++)
+	{
+	}
+	for (j = 0; (j < i && c[j] != '\n'); j++)
+	{
+	if ((c[j] >= 97) && (c[j] <=122))
+		{
+			p[j] = c[j] - 32;
+			c[j] = p[j];
+		}
+	}
+	return (c);
 }
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * leet - function to convert some letters to integers
+ *
+ * @c: string to work on
+ *
+ * Return: char c
+ */
+
+char *leet(char *c)
+{
+	int i;
+	char *p;
+	p = c;
+
+	for (i = 0; c[i] != 0; i++)
+	{
+		if (c[i] == 65 || c[i] == 97)
+		{
+			p[i] = 52;
+			c[i] = p[i];
+		}
+		else if (c[i] == 69 || c[i] == 101)
+		{
+			p[i] = 51;
+			c[i] = p[i];
+		}
+		else if (c[i] == 79 || c[i] == 111)
+		{
+			p[i] = 48;
+			c[i] = p[i];
+		}
+		else if (c[i] == 84 || c[i] == 116)
+		{
+			p[i] = 55;
+			c[i] = p[i];
+		}
+		else if (c[i] == 76 || c[i] == 108)
+		{
+			p[i] = 49;
+			c[i] = p[i];
+		}
+	}
+	/*_putchar('\n');*/
+	return (c);
+}
