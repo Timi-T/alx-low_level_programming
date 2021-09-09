c8fbe8f0e29450212c25a36469562e22b51c8022
Timi-T <timiopebode@gmail.com>
2021-09-09 12:54:59 -0700

    k

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,10 +3,11 @@
 /**
  * string_toupper - function to convert lowercase strings to upper case
  *
+ * @c: string to be operated on
  * Return: char value
  */
 
-char *string_toupper(char *c)
+char *cap_string(char *c)
 {
 	int i;
 	int j;
@@ -18,6 +19,38 @@ char *string_toupper(char *c)
 	}
 	for (j = 0; (j < i && c[j] != '\n'); j++)
 	{
+		if ((c[0] >= 97) && (c[0] <=122))
+		{
+			p[0] = c[0] - 32;
+			c[0] = p[0];
+		}
+		if (c[j] == 32 && (c[j - 1] == 46 || c[j - 1] == 33 || c[j - 1] == 63))
+		{
+			if ((c[j + 1] >= 97) && (c[j + 1] <=122))
+			{
+				p[j + 1] = c[j + 1] - 32;
+				c[j + 1] = p[j + 1];
+			}
+		}
+		if (c[j] == '\n')
+		{
+			if ((c[j + 1] >= 97) && (c[j + 1] <=122))
+			{
+				p[j + 1] = c[j + 1] - 32;
+				c[j + 1] = p[j + 1];
+			}
+		}
+	}
+	return (c);
+}
+/*
+
+
+
+
+
+
+
 	if ((c[j] >= 97) && (c[j] <=122))
 		{
 			p[j] = c[j] - 32;
@@ -25,4 +58,4 @@ char *string_toupper(char *c)
 		}
 	}
 	return (c);
-}
+}*/
