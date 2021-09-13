88d8ae160e3937498479ac8a0661f585fd2545d3
Timi-T <timiopebode@gmail.com>
2021-09-13 10:11:55 -0700

    k

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strchr - function
+ * @s: base string4
+ * @c: substring
+ *
+ * Return: char value
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	int j;
+	int a;
+	char *p;
+
+	p = s;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < i && s[j] != '\0'; j++)
+	{
+		if (s[j] == c)
+		{
+			for (a = 0 ; j < i && s[j] != '\0'; j++, a++)
+			{
+				p[a] = s[j];
+			}
+			p[a] = '\0';
+		}
+	}
+	return (p);
+}
