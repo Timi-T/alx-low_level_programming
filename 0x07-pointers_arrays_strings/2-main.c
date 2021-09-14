8cd0819f3c0f3869c1d5fcff9bdbfaf25180edaa
Timi-T <timiopebode@gmail.com>
2021-09-14 07:25:59 -0700

    k

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -11,7 +11,7 @@ int main(void)
     char *s = "hello, world";
     char *f;
 
-    f = _strchr(s, 'l');
+    f = _strchr(s, 'y');
 
     if (f != NULL)
     {
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,10 +13,10 @@ char *_strchr(char *s, char c)
 	while (*s != '\0' && *s != c)
 	{
 		s++;
-	}
-	if (*s == '\0')
-	{
-		return (0);
+		if (*s == '\0')
+		{
+			return (0);
+		}
 	}
 	return (s);
 }
