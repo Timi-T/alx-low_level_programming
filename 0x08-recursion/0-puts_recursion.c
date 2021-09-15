8ee7956ed4e1bc88995541af6dad4af411d2d87b
Timi-T <timiopebode@gmail.com>
2021-09-15 07:25:16 -0700

    k

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print out a string
+ *
+ * @s: string to print out
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+}
