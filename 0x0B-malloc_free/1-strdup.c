7df66efc61353472232c1a5f684a6aa73f293485
Timi-T <timiopebode@gmail.com>
2021-09-21 05:40:03 -0700

    k

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _stdrup - function to duplicate string
+ *
+ * @str: string to be duplicated
+ *
+ * Return: char value
+ */
+
+char *_stdrup(char *str)
+{
+	int len;
+	int i;
+	char *p;
+
+	for (len = 0; str[len] != 0; len++)
+	{
+	}
+	p = (char *)malloc(len * sizeof(char));
+	for (i = 0; i < len; i++)
+	{
+		*(p + i) = *(str + i);
+	}
+	*(p + len) = '\0';
+	if (len == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (p);
+	}
+	return (0);
+}
