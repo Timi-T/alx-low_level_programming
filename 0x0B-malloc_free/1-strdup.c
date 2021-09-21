9d4f501f0a6f4956bd6d167bfa4d6875e6bb2a0c
Timi-T <timiopebode@gmail.com>
2021-09-21 05:49:58 -0700

    k

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,13 +19,13 @@ char *_strdup(char *str)
 	for (len = 0; str[len] != 0; len++)
 	{
 	}
-	p = (char *)malloc(len * sizeof(char));
+	p = (char *)malloc((len + 1) * sizeof(char));
 	for (i = 0; i < len; i++)
 	{
 		*(p + i) = *(str + i);
 	}
 	*(p + len) = '\0';
-	if (len == 0)
+	if (str == NULL)
 	{
 		return (0);
 	}
