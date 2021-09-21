9e5d96e332818fcc09146722a308c8ccc4941b74
Timi-T <timiopebode@gmail.com>
2021-09-21 04:47:19 -0700

    k

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *p = (char *)malloc(size * sizeof(char));
 
-	*p = c;
+	for (i = 0; i < size; i++)
+	{
+		*(p + i) = c;
+	}
 	if (size == 0)
 	{
 		return (0);
