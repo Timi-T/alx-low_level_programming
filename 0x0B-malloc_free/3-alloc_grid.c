d0ec821195b3b616fa678d2b2455b57a31630afc
Timi-T <timiopebode@gmail.com>
2021-09-22 11:10:28 -0700

    k

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,16 +13,20 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j;
-	int **d;
-	int *p = (int *)malloc(height * width * sizeof(int));
+	int i, j, a;
+	int **p;
 
-	d = &p;
+	p = (int **)malloc(height *  sizeof(int *));
+
+	for (a = 0; a < width; a++)
+	{
+		p[a] = (int *)malloc(sizeof(int) * width);
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
 		{
-			p[i][j] = 0;
+			*(*(p + i) + j) = 0;
 		}
 	}
 	if (width <= 0 || height <= 0)
@@ -31,7 +35,7 @@ int **alloc_grid(int width, int height)
 	}
 	else
 	{
-		return (d);
+		return (p);
 	}
 	return (0);
 }
