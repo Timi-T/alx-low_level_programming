cacc89c3bbf1d5b1d73d44056521eee185bc3963
Timi-T <timiopebode@gmail.com>
2021-09-27 13:29:59 -0700

    k

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - function to print a dog
+ *
+ * @d: poinnter to dog attributes
+ *
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+	{
+		printf("%s", "");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,6 @@ typedef struct dog
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+void print_dog(struct dog *d);
 
 #endif
