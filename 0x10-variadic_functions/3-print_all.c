e723d5cd17b86cd3182ea6e39edc7b8729dc3a0c
Timi-T <timiopebode@gmail.com>
2021-09-30 09:30:35 -0700

    k

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * print_all - function to print any data type
@@ -12,10 +14,20 @@
 
 void print_all(const char * const format, ...)
 {
-	char *p[] = {"char", "int", "float", "char *"};
+	const unsigned int len = strlen(format);
+	long unsigned int i = 0;
+	/*char *selector[] = {"char", "int", "float", "char *"};*/
+	char form[] = {'c', 'i', 'f', 's'};
 	va_list data;
 
-	va_start;
-
-	va_end;
+	va_start(data, format);
+	while (i < len)
+	{
+		if (format[i] == form[i])
+		{
+			printf("%s", va_arg(data, char *));
+		}
+		i++;
+	}
+	va_end(data);
 }
