2df580c42099153ce39e1aecb4dea3020de415ea
Timi-T <timiopebode@gmail.com>
2021-09-30 09:32:51 -0700

    k

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,14 +12,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-	{
-		return (0);
-	}
 	unsigned int i = 0;
 	int sum = 0;
 	va_list arguments;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_start(arguments, n);
 	while (i < n)
 	{
