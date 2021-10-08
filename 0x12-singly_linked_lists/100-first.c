c2f0db0f33b58379ba9c5237d3c6f5cf0c0d2dc4
Timi-T <timiopebode@gmail.com>
2021-10-08 04:32:04 -0700

    k

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * start - constructor
+ *
+ * Return: nothing
+ */
+
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
