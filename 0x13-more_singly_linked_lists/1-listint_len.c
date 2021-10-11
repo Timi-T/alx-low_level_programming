fe068e4c98954dfe27192ee3ccc19a6e5ae8943e
Timi-T <timiopebode@gmail.com>
2021-10-11 04:46:41 -0700

    k

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,21 +10,20 @@
  * Return: the number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
-	const listint_t *first;
+        size_t count = 0;
+        const listint_t *first = h;
 
 	if (h == NULL)
 	{
 		return (-1);
 	}
-	first = h;
 	while (first != NULL)
 	{
-		printf("%d\n", first->n);
-		first = first->next;
 		count++;
+		first = first->next;
 	}
 	return (count);
 }
+
