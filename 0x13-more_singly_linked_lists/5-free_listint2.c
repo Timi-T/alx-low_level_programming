fa95d322dc09b0a717d8413daf1d6cd002227317
Timi-T <timiopebode@gmail.com>
2021-10-12 06:56:43 -0700

    k

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,14 +13,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
-	if (*head == NULL)
-	{
-		exit(0);
-	}
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = temp->next;
 		free(temp);
 	}
+	*head = NULL;
 }
