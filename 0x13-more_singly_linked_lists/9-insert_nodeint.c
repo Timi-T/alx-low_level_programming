c1731b6ff83bbe733f89a42412e8ca278f1574ad
Timi-T <timiopebode@gmail.com>
2021-10-12 00:04:24 -0700

    k

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (h != NULL)
 	{
-		if (count == idx - 1 && h->next != NULL)
+		if (count == idx - 1)
 		{
 			temp = h->next;
 			h->next = new_node;
