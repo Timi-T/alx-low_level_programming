0e66f2bba729fc00b84601df640aa46124de8e2e
Timi-T <timiopebode@gmail.com>
2021-10-16 13:24:09 -0700

    k

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-    print_binary(0);
+    print_binary(-98);
     printf("\n");
-    print_binary(1);
+    /*print_binary(1);
     printf("\n");
     print_binary(98);
     printf("\n");
     print_binary(1024);
     printf("\n");
     print_binary((1 << 10) + 1);
-    printf("\n");
+    printf("\n");*/
     return (0);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct list_bin
+{
+	int n;
+	struct list_bin *next;
+} list_bin;
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned int get_pow(int max_pow_two);
@@ -10,5 +16,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _pow(int base, unsigned int power);
+list_bin *get_binary(unsigned long int n);
 
-#endif /*_HEADER_*/
+#endif
