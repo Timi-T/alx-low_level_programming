1f33605488d821d6da7fa43b8be88eb021cd6758
Timi-T <timiopebode@gmail.com>
2021-10-18 12:09:33 -0700

    k

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,21 +21,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rite;
+	int fd, fdcreate, rite;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd != 1)
+	fdcreate = creat(filename, S_IRUSR | S_IWUSR);
+	if (fdcreate == -1)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
 		rite = write(fd, text_content, strlen(text_content));
-		if (rite != 1)
+		if (rite == -1)
 		{
 			return (-1);
 		}
