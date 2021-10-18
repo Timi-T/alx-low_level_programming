835bc40fc7cad4fcce273b86596fef6aa54bad12
Timi-T <timiopebode@gmail.com>
2021-10-18 04:46:16 -0700

    k

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * read_textfile - function to read a text file
+ *
+ * @filename: name of file to read text from
+ * @letters: size of letters to be read from file
+ *
+ * Return: number of letters read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t size;
+	char *buf;
+	int fd;
+
+	fd = open(filename, O_RDWR);
+	buf  = malloc(letters);
+	size = read(fd, buf, letters);
+	write(1, buf, letters);
+	return (size);
+}
