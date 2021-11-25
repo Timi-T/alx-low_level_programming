16e11c012bea3aa472a03674349378c4c1fea7a0
Timi-T <timiopebode@gmail.com>
2021-11-25 11:16:44 +0000

    readme

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,29 +12,26 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *head_copy;
-	dlistint_t *new_node;
+        dlistint_t *new_node;
 
-	printf("entry point");
-	if (head)
-	{
-		head_copy = *head;
-		while(head_copy->prev != NULL)
-		{
-			head_copy = head_copy->prev;
-		}
-		new_node = malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+        if (*head == NULL)
+        {
 		new_node->prev = NULL;
-		new_node->n = n;
-		new_node->next = head_copy;
-		head_copy->prev = new_node;
+		new_node->next = NULL;
+                *head = new_node;
+		return (new_node);
 	}
 	else
 	{
-		new_node = malloc(sizeof(dlistint_t));
 		new_node->prev = NULL;
-		new_node->n = n;
-		new_node->next = NULL;
+		new_node->next = *head;
+		(*head)->prev = new_node;
+		*head = new_node;
+		return (new_node);
 	}
 	return (new_node);
 }
