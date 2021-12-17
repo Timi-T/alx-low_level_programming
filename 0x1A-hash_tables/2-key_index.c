c9a8b084cfb845bbfcacb05f69e3aa5a57155c27
Timi-T <timiopebode@gmail.com>
2021-12-17 19:02:05 +0000

    include

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,19 @@
-nclude "hash_tables.h"
+#include "hash_tables.h"
 
+/**
+ * key_index - function to retrieve the index of a key
+ *
+ * @key: key to get index of
+ * @size: size of hash table
+ *
+ * Return: index of key
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        unsigned long int index;
-        unsigned long int key_hash;
+	unsigned long int index;
+	unsigned long int key_hash;
 
-        key_hash = hash_djb2(key);
-        index = key_hash % size;
-        return (index);
+	key_hash = hash_djb2(key);
+	index = key_hash % size;
+	return (index);
 }
