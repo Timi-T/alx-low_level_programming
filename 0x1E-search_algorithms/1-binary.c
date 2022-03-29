066bacb68d9d144b0af4b2232220609dbd1b6c60
Timi-T <timiopebode@gmail.com>
2022-03-29 15:03:47 +0000

    slight correction

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "search_algos.h"
 
 /**
  * binary_search - function to search for an element in an array by binary algo
@@ -14,21 +15,10 @@ int binary_search(int *array, size_t size, int value)
 {
 	int half = size / 2;
 	int *arr = array;
-	size_t i = 0;
 
 	if (!arr || !size)
 		return (-1);
-	if (size == 1 && arr[half] == value)
-		return (half);
-	printf("Searching in array: ");
-	while (i < size)
-	{
-		if (i == size - 1)
-			printf("%d\n", arr[i]);
-		else
-			printf("%d, ", arr[i]);
-		i++;
-	}
+	print_message(arr, size);
 	if (arr[half] > value)
 	{
 		return (binary_search(arr, half - 1, value));
@@ -40,6 +30,38 @@ int binary_search(int *array, size_t size, int value)
 		return (binary_search(&(arr[half]), size - half, value));
 	}
 	else
+	{
+		if (size > 2)
+			print_message(&(arr[half]), size -half);
 		return (half);
+	}
 	return (-1);
+}
+
+/**
+ * print_message - function to print a message
+ *
+ * @array: array to get information from
+ * @size: size of array
+ *
+ * Return: nothing
+ */
+void print_message(int *array, size_t size)
+{
+	size_t i = 0;
+
+	printf("Searching in array: ");
+	while (i < size)
+	{
+		if (i == size - 1)
+		{
+			printf("%d\n", array[i]);
+			return;
+		}
+		else
+		{
+			printf("%d, ", array[i]);
+			i++;
+		}
+	}
 }
